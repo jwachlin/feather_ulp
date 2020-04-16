4d34c8f152b6684f25246bfdb205db921d70ce71
jwachlin <16798559+jwachlin@users.noreply.github.com>
2020-04-16 19:29:54 -0400

    Uses performance level 0 and buck converter for reduced current consumption. 475uA at 3.3V measured.

--- Feather_ULP/Feather_ULP/src/main.c
+++ Feather_ULP/Feather_ULP/src/main.c
@@ -18,6 +18,16 @@ int main (void)
 
 	delay_ms(50);
 
+	// Set up to performance level 0, buck converter
+	// Clock must be <12MHz, other clocks must meet specs in datasheet
+	system_switch_performance_level(SYSTEM_PERFORMANCE_LEVEL_0); 
+
+	struct system_voltage_regulator_config vreg_config;
+	system_voltage_regulator_get_config_defaults(&vreg_config);
+	vreg_config.regulator_sel = SYSTEM_VOLTAGE_REGULATOR_BUCK;
+	vreg_config.low_power_efficiency = SYSTEM_VOLTAGE_REGULATOR_LOW_POWER_EFFICIENCY_HIGHTEST; // Higher efficiency, runs 2.5-3.6V only
+	system_voltage_regulator_set_config(&vreg_config);
+
 	i2c_interface_init();
 	init_lis3dh();
 	init_bmp280();
